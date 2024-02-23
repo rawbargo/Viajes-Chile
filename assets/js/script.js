e32b03f07d6d4a4659275fa6c4060de77cb56fa9
Ro <rodgonzalez89@gmail.com>
2024-02-22 21:05:24 -0300

    ahora quienes somos cambia de color al infinito

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,25 @@ function myFunction() {
     alert("El correo ha sido enviadors");
   }
  
+  function cambiarColor(idElemento) {
+    var elemento = document.getElementById(idElemento);
+    var colorActual = elemento.style.color;
+    var nuevoColor = '';
+
+    
+    do {
+      nuevoColor = getRandomColor();
+    } while (nuevoColor === colorActual); 
+
+    elemento.style.color = nuevoColor;
+  }
+
+  
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
